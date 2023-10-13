a9676cf967ae21bf96af8b0d356f2e5d3630f5a1
priyeshkadbe <priyeshkadbe49@gmail.com>
2023-10-13 06:06:43 +0530

    added swagger docs

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ const apiRoutes = require("./routes/index");
 const { PORT } = require("./config/serverConfig");
 const cors = require("cors");
 const { default: axios } = require("axios");
-const YAML = require("yamljs")
-const swaggerDocument = YAML.load("../api.yaml");
-const swaggerSpec = require("./config/swaggerSpec");
+// const YAML = require("yamljs")
+// const swaggerDocument = YAML.load("../api.yaml");
+// const swaggerSpec = require("./config/swaggerSpec");
 const setupAndStartServer = () => {
   const app = express();
   app.use(cors());
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: "true" }));
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  //app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use("/api", apiRoutes);
   app.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`);
